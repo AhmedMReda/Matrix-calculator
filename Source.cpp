41630ef6e785eb60fc3839e950602bcc071df1b6
AhmedMReda <84842935+AhmedMReda@users.noreply.github.com>
2022-04-14 23:22:50 +0200

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 using namespace std;
-void main()
+int main()
 {
 	float a1, a2, a3, a4, a5, a6, a7, a8, a9, b1, b2, b3, b4, b5, b6, b7, b8, b9, detA, detB, A1, A2, A3, A4, A5, A6, A7, A8, A9, B1, B2, B3, B4, B5, B6, B7, B8, B9;
 	string again1, again2;
